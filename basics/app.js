96a2d13ac2cd1805c98aba003966ff8e832b7c76
Rohitwable <rohitwable46@gmail.com>
2020-01-19 15:06:33 +0530

    create form and add redirection based on the condition of method an url

--- basics/app.js
+++ basics/app.js
@@ -1,5 +1,6 @@
 //import core node modules
 const http = require('http');
+const fs = require('fs');
 
 // create server
 const server = http.createServer((req, res) => {
@@ -8,6 +9,25 @@ const server = http.createServer((req, res) => {
     //set headers to the response
     res.setHeader('Content-type','text/html');
     //write data to the response
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Node js app</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="test" name="message" placeholder="Enter text"/><button type="submit">Submit</button></form></body>');
+        res.write('<html/>');  
+        return res.end();  // quite the function after returning
+    }
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'DUMMY');
+        
+        //send status code
+        res.statusCode = 302;
+        //send location header for redirection
+        res.setHeader('Location', '/');
+        return res.end();
+        
+    }
     res.write('<html>');
     res.write('<head><title>Node js app</title></head>');
     res.write('<body>Node js app</body>');
