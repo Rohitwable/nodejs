f294f7530b2c2d7ccda7db61df6640a7c3cedeaf
Rohitwable <rohitwable46@gmail.com>
2020-01-19 17:33:48 +0530

    module export import

--- basics/app.js
+++ basics/app.js
@@ -1,52 +1,10 @@
 //import core node modules
 const http = require('http');
-const fs = require('fs');
 
+//import local modules
+const routes = require('./routes');
 // create server
-const server = http.createServer((req, res) => {
-
-    //set headers to the response
-    res.setHeader('Content-type', 'text/html');
-    //write data to the response
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Node js app</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="test" name="message" placeholder="Enter text"/><button type="submit">Submit</button></form></body>');
-        res.write('<html/>');
-        return res.end();  // quite the function after returning
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk)
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, error => {
-                //send status code
-                res.statusCode = 302;
-                //send location header for redirection
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        })
-
-    }
-    res.write('<html>');
-    res.write('<head><title>Node js app</title></head>');
-    res.write('<body>Node js app</body>');
-    res.write('<html/>');
-
-    //end the response
-
-    res.end();
-    //can not send response to client after ending it
-});
+const server = http.createServer(routes);
 
 //listen to the port
 server.listen(3002);
---
+++ basics/routes.js
@@ -0,0 +1,47 @@
+//import core modules
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Node js app</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="test" name="message" placeholder="Enter text"/><button type="submit">Submit</button></form></body>');
+        res.write('<html/>');
+        return res.end();  // quite the function after returning
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk)
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, error => {
+                //send status code
+                res.statusCode = 302;
+                //send location header for redirection
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        })
+
+    }
+    res.write('<html>');
+    res.write('<head><title>Node js app</title></head>');
+    res.write('<body>Node js app</body>');
+    res.write('<html/>');
+
+    //end the response
+
+    res.end();
+    //can not send response to client after ending it
+
+}
+
+//export the module
+module.exports = requestHandler;
