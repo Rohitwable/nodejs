138b8910928a5edb2b746c70040949b8669180ad
Rohitwable <rohitwable46@gmail.com>
2020-01-19 15:30:25 +0530

    listening to the events / read data and send it to buffer / write data stored in buffer to file

--- basics/app.js
+++ basics/app.js
@@ -4,35 +4,43 @@ const fs = require('fs');
 
 // create server
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
 
     //set headers to the response
-    res.setHeader('Content-type','text/html');
+    res.setHeader('Content-type', 'text/html');
     //write data to the response
     const url = req.url;
     const method = req.method;
-    if(url === '/'){
+    if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Node js app</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="test" name="message" placeholder="Enter text"/><button type="submit">Submit</button></form></body>');
-        res.write('<html/>');  
+        res.write('<html/>');
         return res.end();  // quite the function after returning
     }
-    if(url === '/message' && method === 'POST'){
-        fs.writeFileSync('message.txt', 'DUMMY');
-        
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk)
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
         //send status code
         res.statusCode = 302;
         //send location header for redirection
         res.setHeader('Location', '/');
         return res.end();
-        
+
     }
     res.write('<html>');
     res.write('<head><title>Node js app</title></head>');
     res.write('<body>Node js app</body>');
     res.write('<html/>');
-    
+
     //end the response
 
     res.end();
